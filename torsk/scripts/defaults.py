35000104ad9b13249b0007b413e74f9fdf99de6b
Niklas Heim <heim.niklas@gmail.com>
2018-11-14 23:12:07 +0100

    default params.json creation cli

---
+++ torsk/scripts/defaults.py
@@ -0,0 +1,23 @@
+import pathlib
+from shutil import copyfile
+import click
+
+
+_module_dir = pathlib.Path(__file__).absolute().parent
+
+
+@click.command('params',
+    short_help='Creates a default params.json for different models')
+@click.argument('model_name', type=click.Choice(['mackey', 'kuro']))
+@click.option('--outfile', '-o', type=pathlib.Path,
+    default=pathlib.Path('params.json'),
+    help='Path to save the params.json file')
+def cli(model_name, outfile):
+    """Get the default params.json for the given MODEL_NAME and save if to PATH"""
+    if model_name == 'mackey':
+        params_json = _module_dir / 'mackey_default_params.json'
+    elif model_name == 'kuro':
+        params_json = _module_dir / 'kuro_default_params.json'
+    else:
+        raise ValueError(f"No default params for model: {model_name}")
+    copyfile(params_json, outfile)
