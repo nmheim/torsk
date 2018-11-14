68169ad39ea339e11f32432e9aee9f0486b68509
Niklas Heim <heim.niklas@gmail.com>
2018-11-14 23:11:00 +0100

    good mackey parameters with tikhonov reg.

--- experiments/mackey/hpopt.py
+++ experiments/mackey/hpopt.py
@@ -12,26 +12,26 @@ from torsk.data import MackeyDataset, SeqDataLoader
 
 opt_steps = 100
 output_dir = pathlib.Path("hpopt")
-train_length = 2200
-pred_length = 500
-transient_length = 200
-
 
 dimensions = [
     Real(low=0.5, high=2.0, name="spectral_radius"),
-    Real(low=0.1, high=1.0, name="in_weight_init"),
-    Real(low=0.1, high=1.0, name="in_bias_init"),
-    Real(low=0.006, high=1.0, name="density", prior="log_scale")
+    Real(low=0.01, high=2.0, name="in_weight_init"),
+    Real(low=0.01, high=2.0, name="in_bias_init"),
+    #Real(low=1e-5, high=1e1, name="tikhonov_beta", prior="log_scale")
 ]
 
 starting_params = [
     1.3,    # esn_spectral_radius
     0.5,    # in_weight_init
     0.5,    # in_bias_init
-    0.1,    # esn_density
+    #1.0,    # tikhonov_beta
 ]
 
-params = Params("params.json")
+params = Params("hpopt_params.json")
+train_length = params.train_length
+pred_length = params.pred_length
+transient_length = params.transient_length
+
 # input/label setup
 dataset = MackeyDataset(
     seq_length=train_length + pred_length,
@@ -48,7 +48,7 @@ def fitness(**sampled_params):
 
 
     error = []
-    for _ in range(10):
+    for _ in range(5):
         inputs, labels = next(loader)
         train_inputs, train_labels = inputs[:train_length], labels[:train_length]
         test_inputs = inputs[train_length - transient_length:train_length]
@@ -58,15 +58,19 @@ def fitness(**sampled_params):
         state = torch.zeros(1, params.hidden_size)
         _, states = model(train_inputs, state)
         model.train(
-            states[transient_length:, 0], train_labels[transient_length:, 0])
-        
+            inputs=train_inputs[transient_length:, 0],
+            states=states[transient_length:, 0],
+            labels=train_labels[transient_length:, 0],
+            method=params.train_method,
+            beta=params.tikhonov_beta)
+         
         # predict
         state = torch.zeros(1, params.hidden_size)
         outputs, _ = model(test_inputs, state, nr_predictions=pred_length)
 
         err = (test_labels[transient_length:] - outputs[transient_length:])**2
         error.append(err)
-    metric = torch.mean(torch.cat(error, dim=0))**.5
+    metric = torch.mean(torch.cat(error, dim=0))
     if not torch.isfinite(metric):
         metric = 1e6
     else:
--- experiments/mackey/run.py
+++ experiments/mackey/run.py
@@ -7,46 +7,48 @@ from torsk.utils import Params
 from torsk.data import MackeyDataset, SeqDataLoader
 
 
-train_length = 2200
-pred_length = 500
-transient_length = 200
+params = Params("params.json")
+
+train_length = params.train_length
+pred_length = params.pred_length
+transient_length = params.transient_length
+beta = params.tikhonov_beta
+method = params.train_method
+print(params)
 
 # input/label setup
 dataset = MackeyDataset(train_length + pred_length, simulation_steps=3000)
-loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=False))
+loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=True))
 
-# build model
-params = Params("params.json")
-print(params)
-
+model = ESN(params)
 
 for i in range(10):
 
-    model = ESN(params)
-
-
     inputs, labels = next(loader)
-    
-    
+
     train_inputs, train_labels = inputs[:train_length], labels[:train_length]
     test_inputs = inputs[train_length - transient_length:train_length]
     test_labels = labels[train_length - transient_length:]
-    
+
     # create states and train
     state = torch.zeros(1, params.hidden_size)
-    
+
     _, states = model(train_inputs, state)
     
     model.train(
         inputs=train_inputs[transient_length:, 0],
         states=states[transient_length:, 0],
         labels=train_labels[transient_length:, 0],
-        method='tikhonov',
-        beta=1e0)
+        method=method,
+        beta=beta)
     
     # predict
     state = torch.zeros(1, params.hidden_size)
     outputs, _ = model(test_inputs, state, nr_predictions=pred_length)
+
+    err = (test_labels[transient_length:] - outputs[transient_length:])**2
+    mse = torch.mean(err)
+    print(f"RMSE: {mse.item()}")
     
     fig, ax = plt.subplots(2,1)
     ax[0].plot(test_labels.numpy()[transient_length:, 0, 0])
