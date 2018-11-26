1807672876c3db5e2e70cf705f919d333f4f9081
James Avery <avery@diku.dk>
2018-11-26 17:28:06 +0100

    Parameterized resolution for ocean run-script

--- experiments/ocean/run.py
+++ experiments/ocean/run.py
@@ -9,13 +9,17 @@ from torsk.models import ESN
 from torsk.data import NetcdfDataset, SeqDataLoader
 from torsk.visualize import animate_double_imshow
 
+Nx, Ny = 80, 80
+
 params = torsk.Params("params.json")
+params.input_size  = Nx*Ny
+params.output_size = Nx*Ny
 print(params)
 
 print("Loading + resampling of kuro window ...")
 ncpath = pathlib.Path('../../data/ocean/kuro_SSH_3daymean_scaled.nc')
 dataset = NetcdfDataset(ncpath, params.train_length, params.pred_length,
-    xslice=slice(90, 190), yslice=slice(90, 190), size=[30, 30])
+    xslice=slice(90, 190), yslice=slice(90, 190), size=[Nx, Ny])
 loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=True))
 
 print("Building model ...")
@@ -30,8 +34,8 @@ hist, bins = np.histogram(weight, bins=100)
 plt.plot(bins[1:], hist)
 plt.show()
 
-labels = pred_labels.numpy().reshape([-1, 30, 30])
-outputs = outputs.numpy().reshape([-1, 30, 30])
+labels = pred_labels.numpy().reshape([-1, Nx, Ny])
+outputs = outputs.numpy().reshape([-1, Nx, Ny])
 anim = animate_double_imshow(labels, outputs)
 plt.show()
 
