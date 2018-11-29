163441d9a6a2330c395eef2650f1c2086ec925d1
Niklas Heim <heim.niklas@gmail.com>
2018-11-29 21:28:05 +0100

    update run scripts

--- experiments/mackey/run.py
+++ experiments/mackey/run.py
@@ -24,14 +24,16 @@ dataset = MackeyDataset(train_length, pred_length, simulation_steps=3000)
 loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=True))
 
 model = ESN(params)
+model.eval()
 
 predictions, labels = [], []
 for i in tqdm(range(20)):
-    model, outputs, pred_labels = torsk.train_predict_esn(
+
+    model, outputs, pred_labels, _ = torsk.train_predict_esn(
         model=model, loader=loader, params=params)
+
     predictions.append(outputs.squeeze().numpy())
     labels.append(pred_labels.squeeze().numpy())
-
 
 predictions, labels = np.array(predictions), np.array(labels)
 
--- experiments/ocean/run.py
+++ experiments/ocean/run.py
@@ -1,3 +1,4 @@
+import logging
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,23 +10,27 @@ from torsk.models import ESN
 from torsk.data import NetcdfDataset, SeqDataLoader
 from torsk.visualize import animate_double_imshow
 
+
+logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.DEBUG)
+
 Nx, Ny = 80, 80
 
 params = torsk.Params("params.json")
 params.input_size  = Nx*Ny
 params.output_size = Nx*Ny
-print(params)
+logger.info(params)
 
-print("Loading + resampling of kuro window ...")
+logger.info("Loading + resampling of kuro window ...")
 ncpath = pathlib.Path('../../data/ocean/kuro_SSH_3daymean_scaled.nc')
 dataset = NetcdfDataset(ncpath, params.train_length, params.pred_length,
     xslice=slice(90, 190), yslice=slice(90, 190), size=[Nx, Ny])
 loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=True))
 
-print("Building model ...")
+logger.info("Building model ...")
 model = ESN(params)
 
-print("Training + predicting ...")
+logger.info("Training + predicting ...")
 model, outputs, pred_labels = torsk.train_predict_esn(model, loader, params)
 
 weight = model.esn_cell.res_weight._values().numpy()
