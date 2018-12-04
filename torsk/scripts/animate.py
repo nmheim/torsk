c671ea4b9124a0de73b1482776e85119a6969599
Niklas Heim <heim.niklas@gmail.com>
2018-12-04 23:31:12 +0100

    add nc-variable to animate cli

--- torsk/scripts/animate.py
+++ torsk/scripts/animate.py
@@ -6,10 +6,11 @@ from torsk.visualize import animate_imshow
 
 
 @click.command("animate", short_help="Animate the SSH component of a .nc file")
+@click.option("--variable", "-v", type=str, default="SSH")
 @click.argument("ncfile", type=pathlib.Path)
-def cli(ncfile):
+def cli(ncfile, variable):
     with nc.Dataset(ncfile, "r") as src:
-        data = src["SSH"][:]
+        data = src[variable][:]
 
     anim = animate_imshow(data)  # NOQA
     plt.show()
