1fb39ddffc5185636025a518cad2d5c1145b0320
Niklas Heim <heim.niklas@gmail.com>
2018-12-04 23:35:14 +0100

    sine script updates with new torsk.hpopt function

--- experiments/sine/hpopt.py
+++ experiments/sine/hpopt.py
@@ -1,97 +1,73 @@
 from datetime import datetime
 import pathlib
+import logging
+
 import numpy as np
-import torch
 import skopt
 from skopt.utils import use_named_args
 from skopt.space import Real, Integer
 
 import torsk
-from torsk.models import ESN
-from torsk.utils import Params
 from torsk.data import SineDataset, SeqDataLoader
+from torsk.hpopt import esn_tikhonov_fitnessfunc
+
+
+logging.basicConfig(level="INFO")
 
 
 opt_steps = 50
+tik_steps = 20
+tik_start = -5
+tik_stop = 2
+tikhonov_betas = np.logspace(tik_start, tik_stop, tik_steps)
+
 output_dir = pathlib.Path("hpopt")
 
 dimensions = [
     Real(low=0.5, high=2.0, name="spectral_radius"),
-    Real(low=0.001, high=2.0, name="in_weight_init", prior="log_scale"),
-    Real(low=0.001, high=2.0, name="in_bias_init", prior="log_scale"),
-    # Integer(low=10, high=200, name="hidden_size"),
-    # Real(low=1e-5, high=1e1, name="tikhonov_beta", prior="log_scale")
+    Real(low=0.0, high=2.0, name="in_weight_init"),
+    Real(low=0.0, high=2.0, name="in_bias_init"),
 ]
 
 starting_params = [
     1.0,    # esn_spectral_radius
     0.01,    # in_weight_init
     0.01,    # in_bias_init
-    # 100,     # hidden size
-    # 0.01,    # tikhonov_beta
 ]
 
-params = Params("hpopt_params.json")
-
+params = torsk.Params("hpopt_params.json")
 dataset = SineDataset(
     train_length=params.train_length,
     pred_length=params.pred_length)
 loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=True))
 
+fitness = esn_tikhonov_fitnessfunc(loader, params, dimensions, tikhonov_betas)
 
-@use_named_args(dimensions=dimensions)
-def fitness(**sampled_params):
+# TODO: add callback that saves checkpoints
+result = skopt.gp_minimize(
+    n_calls=opt_steps,
+    func=fitness,
+    dimensions=dimensions,
+    acq_func="gp_hedge",
+    x0=starting_params,
+    verbose=True)
 
-    params.update(sampled_params)
-    # params.hidden_size = int(sampled_params['hidden_size'])
-    print(f"Current model parameters {params}")
-    model = ESN(params)
+print("\n\nBest parameters:")
+keys = [d.name for d in dimensions]
+for key, val in zip(keys, result.x):
+    print("\t"+key, val)
+print("With loss:", result.fun)
+print("\n")
 
+sorted_results = sorted(zip(result.func_vals, result.x_iters))
+for res in sorted_results:
+    print(res)
 
-    predictions, labels = [], []
-    for _ in range(30):
-        model, outputs, pred_labels = torsk.train_predict_esn(
-            model=model, loader=loader, params=params)
-        predictions.append(outputs.squeeze().numpy())
-        labels.append(pred_labels.squeeze().numpy())
+if not output_dir.exists():
+    output_dir.mkdir(parents=True)
 
-    predictions, labels = np.array(predictions), np.array(labels)
-    error = (predictions - labels)**2
-
-    metric = np.mean(error)
-    if not np.isfinite(metric):
-        metric = 1e6
-    return metric
-
-
-if __name__ == "__main__":
-    
-    # TODO: add callback that saves checkpoints
-    result = skopt.gp_minimize(
-        n_calls=opt_steps,
-        func=fitness,
-        dimensions=dimensions,
-        acq_func="EI",
-        x0=starting_params,
-        verbose=True)
-
-    print("\n\nBest parameters:")
-    keys = [d.name for d in dimensions]
-    for key, val in zip(keys, result.x):
-        print("\t"+key, val)
-    print("With loss:", result.fun)
-    print("\n")
-
-    sorted_results = sorted(zip(result.func_vals, result.x_iters))
-    for res in sorted_results:
-        print(res)
-
-    if not output_dir.exists():
-        output_dir.mkdir(parents=True)
-
-    now = datetime.now().strftime("%Y%m%d-%H%M%S")
-    skopt.dump(
-        result,
-        output_dir.joinpath(f"result_{now}.pkl"),
-        store_objective=False)
-
+now = datetime.now().strftime("%Y%m%d-%H%M%S")
+skopt.dump(
+    result,
+    output_dir.joinpath(f"result_{now}.pkl"),
+    store_objective=False)
--- experiments/sine/run.py
+++ experiments/sine/run.py
@@ -11,22 +11,24 @@ from torsk.data import SineDataset, SeqDataLoader
 
 # Parameters and model initialization
 params = torsk.Params("params.json")
+print(params)
 
 dataset = SineDataset(
     train_length=params.train_length,
     pred_length=params.pred_length)
 loader = iter(SeqDataLoader(dataset, batch_size=1, shuffle=True))
 
-model = ESN(params)
-
 predictions, labels = [], []
 for i in tqdm(range(30)):
+    model = ESN(params)
     model, outputs, pred_labels, _ = torsk.train_predict_esn(
         model=model, loader=loader, params=params)
     predictions.append(outputs.squeeze().numpy())
     labels.append(pred_labels.squeeze().numpy())
 
 predictions, labels = np.array(predictions), np.array(labels)
+error = (predictions - labels)**2
+print(f"Metric: {error.mean()}")
 
 plot_mackey(predictions, labels, weights=model.out.weight.numpy())
 plt.show()
