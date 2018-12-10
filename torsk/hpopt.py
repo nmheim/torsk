e395b3ce838626705368c338268258da3c83bd19
Niklas Heim <heim.niklas@gmail.com>
2018-12-11 00:02:26 +0100

    new hyper parameter functions to follow folder structure of issue #20

--- torsk/hpopt.py
+++ torsk/hpopt.py
@@ -1,12 +1,116 @@
+import pathlib
 import logging
+
 import numpy as np
 from skopt.utils import use_named_args
 import torch
 from tqdm import tqdm
-from torsk.models import ESN
 
+from torsk.models import ESN
+from torsk import utils
 
 logger = logging.getLogger(__name__)
+
+
+SECOND_LEVEL_HYPERPARAMETERS = [
+    "sigma", "domain", "transient_length", "pred_length"
+]
+
+
+def is_second_level_param(param_name):
+    return param_name in SECOND_LEVEL_HYPERPARAMETERS
+
+
+def valid_second_level_params(params):
+    for name in params:
+        if not is_second_level_param(name):
+            return False
+    return True
+
+
+def evaluate_hyperparams(outdir, default_params, hyper_params, loader, tikhonov_betas):
+    if not isinstance(outdir, pathlib.Path):
+        outdir = pathlib.Path(outdir)
+    if not outdir.exists():
+        outdir.mkdir(parents=True)
+
+    logger.info("Building model")
+    model = ESN(default_params)
+
+    logger.info(f"Dumping model at {outdir}/[params.json, model.pth]")
+    utils.save_model(outdir, model)
+
+    logger.info("Loading dataset")
+    inputs, labels, pred_labels, orig_data = next(loader)
+
+    logger.info(f"Creating {inputs.size(0)} training states")
+    states = utils.create_training_states(model, inputs)
+
+    # TODO: save orig_data?
+    train_data_nc = outdir / "train_data.nc"
+    logger.info(f"Dumping training data at {train_data_nc}")
+    utils.dump_training(
+        fname=train_data_nc,
+        inputs=inputs.reshape([-1, model.params.input_size]),
+        labels=labels.reshape([-1, model.params.output_size]),
+        states=states.squeeze(),
+        pred_labels=pred_labels.reshape([-1, model.params.output_size]))
+
+    for hyper in hyper_params:
+        if valid_second_level_params(hyper):
+            model.params.update(hyper)
+            level2_outdir = utils.create_path(outdir, hyper)
+            optimize_wout(
+                outdir=level2_outdir, model=model, inputs=inputs,
+                states=states, train_labels=labels, pred_labels=pred_labels,
+                tikhonov_betas=tikhonov_betas)
+        else:
+            raise ValueError(
+                f"Not all hyper_params are of second level: {hyper_params}")
+
+
+def optimize_wout(outdir, model, inputs, states, train_labels, pred_labels, tikhonov_betas):
+    """Run optimization of last ESN layer (Wout) both with Pseudo Inverse and
+    Tikhonov with a list of regularization parameter values."""
+    if not isinstance(outdir, pathlib.Path):
+        outdir = pathlib.Path(outdir)
+
+    tlen = model.params.transient_length
+    plen = model.params.pred_length
+    init_input = train_labels[-1]
+    init_state = states[-1]
+
+    def _optimize(model, method, beta):
+        model.params.train_method = method
+        model.params.tikhonov_beta = beta
+        logger.info(f"Optimizing Wout with method:{method} / beta:{beta}")
+        model.optimize(
+            inputs=inputs[tlen:], states=states[tlen:], labels=train_labels[tlen:])
+
+        logger.info(f"Predicting {plen} steps")
+        outputs, out_states = model.predict(
+            init_input, init_state, nr_predictions=plen)
+
+        if method == "pinv":
+            method_outdir = outdir / "pinv"
+        elif method == "tikhonov":
+            method_outdir = outdir / f"tik{beta}"
+
+        pred_data_nc = method_outdir / "pred_data.nc"
+        logger.info(f"Dumping prediction data at {pred_data_nc}")
+        utils.dump_prediction(
+            fname=pred_data_nc,
+            outputs=outputs.reshape([-1, model.params.output_size]),
+            labels=pred_labels.reshape([-1, model.params.output_size]),
+            states=out_states.squeeze())
+
+        logger.info(f"Dumping model at {method_outdir}/[params.json, model.pth]")
+        utils.save_model(method_outdir, model)
+
+    _optimize(model, method="pinv", beta=None)
+
+    for beta in tikhonov_betas:
+        _optimize(model, method="tikhonov", beta=beta)
 
 
 def _tikhonov_optimize(tikhonov_betas, model, params, inputs, states, labels, pred_labels):
