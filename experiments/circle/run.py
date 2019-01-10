0e250d05ad87e1312870083d7a25341002b83887
Niklas Heim <heim.niklas@gmail.com>
2019-01-10 23:34:04 +0100

    update circle run script

--- experiments/circle/run.py
+++ experiments/circle/run.py
@@ -7,8 +7,6 @@ import torsk
 from torsk.data.utils import gauss2d_sequence, mackey_sequence
 from torsk.visualize import animate_double_imshow
 
-logger = logging.getLogger(__file__)
-logging.basicConfig(level=logging.INFO)
 
 
 def update_params(params,args):
@@ -19,7 +17,7 @@ def update_params(params,args):
 
 params = torsk.Params()
 params.input_map_specs = [
-    # {"type": "pixels", "size": [20, 20], "input_scale": 9.},
+    # {"type": "pixels", "size": [20, 20], "input_scale": 4.},
     # {"type": "conv", "size": [10, 10], "kernel_type":"random", "input_scale": 1.},
     # {"type": "conv", "size": [10, 10], "kernel_type":"random", "input_scale": 1.},
     # {"type": "conv", "size": [10, 10], "kernel_type":"random", "input_scale": 1.},
@@ -31,36 +29,44 @@ params.input_map_specs = [
     # {"type": "conv", "size": [5, 5], "kernel_type":"gauss", "input_scale": 9.},
     # {"type": "conv", "size": [5, 5], "kernel_type":"mean", "input_scale": 9.},
     # {"type": "dct", "size": [20, 20], "input_scale": 1.},
-    {"type": "random_weights", "size": 2500, "weight_scale": 1.0}
+    {"type": "random_weights", "size": [1000], "weight_scale": 0.125}
 ]
-params.spectral_radius = 1.5
+
+params.spectral_radius = 2.0
 params.density = 0.01
 params.input_shape = [30, 30]
 params.train_length = 2000
 params.pred_length = 300
 params.transient_length = 200
-params.dtype = "float32"
+params.dtype = "float64"
 params.reservoir_representation = "sparse"
-params.backend = "torch"
+params.backend = "numpy"
 params.train_method = "pinv"
 params.tikhonov_beta = 0.01
+params.debug = True
 
 update_params(params,sys.argv[1:]);
 
+logger = logging.getLogger(__file__)
+level = "DEBUG" if params.debug else "INFO"
+logging.basicConfig(level=level)
+logging.getLogger("matplotlib").setLevel("INFO")
+
 if params.backend == "numpy":
     logger.info("Running with NUMPY backend")
-    from torsk.data.numpy_dataset import NumpyRawImageDataset as ImageDataset
+    from torsk.data.numpy_dataset import NumpyImageDataset as ImageDataset
     from torsk.models.numpy_esn import NumpyESN as ESN
 else:
     logger.info("Running with TORCH backend")
-    from torsk.data.torch_dataset import TorchRawImageDataset as ImageDataset
+    from torsk.data.torch_dataset import TorchImageDataset as ImageDataset
     from torsk.models.torch_esn import TorchESN as ESN
 
 logger.info(params)
 
 logger.info("Creating circle dataset ...")
 t = np.arange(0, 200*np.pi, 0.1)
-x, y = np.sin(0.25 * t), np.cos(t)
+# x, y = np.sin(0.3 * t), np.cos(t)
+x, y = np.sin(t), np.cos(0.3 * t)
 # y = mackey_sequence(N=t.shape[0]) * 2 - 1
 center = np.array([y, x]).T
 images = gauss2d_sequence(center, sigma=0.5, size=params.input_shape)
@@ -87,8 +93,12 @@ logger.info("Visualizing results ...")
 
 # real_pixels      = dataset.to_images(pred_labels)
 # predicted_pixels = dataset.to_images(outputs)
-real_pixels = pred_labels.squeeze().numpy()
-predicted_pixels = outputs.squeeze().numpy()
+if params.backend == "torch":
+    real_pixels = pred_labels.squeeze().numpy()
+    predicted_pixels = outputs.squeeze().numpy()
+else:
+    real_pixels = pred_labels
+    predicted_pixels = outputs
 
 y, x = 5, 5
 plt.plot(real_pixels[:, y, x])
