1ef616fd8f4f2ff139866d37da3d6910898dc9bf
James Avery <avery@diku.dk>
2019-01-14 17:26:49 +0100

    Playing with Mackey

--- experiments/circle/conv_run.py
+++ experiments/circle/conv_run.py
@@ -32,7 +32,9 @@ params.input_map_specs = [
     {"type": "conv", "size": [15, 15], "kernel_type":"gauss", "input_scale": 1.},
     {"type": "conv", "size": [20, 20], "kernel_type":"gauss", "input_scale": 1.},
     {"type": "conv", "size": [25, 25], "kernel_type":"gauss", "input_scale": 1.},        
-    {"type": "conv", "size": [15, 15], "kernel_type":"random", "input_scale": 1.},
+    {"type": "conv", "size": [ 5, 5], "kernel_type":"random", "input_scale": 1.},
+    {"type": "conv", "size": [10, 10], "kernel_type":"random", "input_scale": 1.},        
+    {"type": "conv", "size": [20, 20], "kernel_type":"random", "input_scale": 1.},    
     # {"type": "conv", "size": [5, 5], "kernel_type":"random", "input_scale": 1.},
     # {"type": "conv", "size": [5, 5], "kernel_type":"random", "input_scale": 1.},
     # {"type": "conv", "size": [5, 5], "kernel_type":"random", "input_scale": 1.},
@@ -51,9 +53,10 @@ params.input_map_specs = [
      {"type": "dct", "size": [20, 20], "input_scale": 1.},
     # {"type": "dct", "size": [10, 10], "input_scale": 1.},
 #    {"type": "random_weights", "size": [2000], "weight_scale": 1, "input_scale":0.05}
+#    {"type": "random_weights", "size": [30*30], "weight_scale": 1, "input_scale":0.025}    
 ]
 
-params.spectral_radius = 2.0
+params.spectral_radius = 2.
 params.density = 0.01
 params.input_shape = [30, 30]
 params.train_length = 2000
@@ -86,8 +89,8 @@ logger.info(params)
 
 logger.info("Creating circle dataset ...")
 t = np.arange(0, 200*np.pi, 0.1)
-#x, y = np.sin(t), np.cos(0.3 * t)
-x, y = np.sin(0.3 * t), np.cos(t)
+x, y = np.sin(t), np.cos(0.3 * t)
+#x, y = np.sin(0.3 * t), np.cos(t)
 x = normalize(mackey_sequence(N=t.shape[0])) * 2 - 1
 
 center = np.array([y, x]).T
