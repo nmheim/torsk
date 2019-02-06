989ac3523f5faa143ee5a7b937a7fe9e6d29a53a
James Avery <avery@diku.dk>
2019-02-06 15:52:48 +0100

    WIP: Making detrending code more robust

--- torsk/data/detrend.py
+++ torsk/data/detrend.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.linalg as la
 from torsk.data.utils import upscale, downscale
+from scipy.fftpack import dctn, idctn
 
 def polynomial_trend(ft,d):
     """Finds best least-squares 'd'-degree polynomial fitting the series 'ft'."""
@@ -15,6 +16,20 @@ def cycles(ft,cycle_length):
     n_cycles  = n//cycle_length;
     nC = n_cycles*cycle_length;
     return ft[:nC].reshape(n_cycles,cycle_length), ft[nC:];
+
+def sine_average(fC, nk):
+    C            = np.mean(fC,axis=0);
+    cycle_length = len(C);
+    cycle_thetas = np.linspace(0,np.pi,cycle_length,endpoint=False);
+    ks           = np.arange(nk);
+    sine_basis   = np.sin(ks[:,None]*cycle_thetas[None,:]);
+#    print(sine_basis.shape, C.shape);
+    cm = la.lstsq(sine_basis.T,C)[0];
+
+#    print(cm.shape)
+    
+    return np.dot(sine_basis.T,cm);
+
 
 def separate_trend_scaled(ft,nT,Cycle_length):
     """Separate out the quadratic trend and average cycle from a time series 'ft'.
@@ -42,8 +57,9 @@ def separate_trend_scaled(ft,nT,Cycle_length):
     # Remove average cycle
     fT=upscale(ft-trend,nT);
     fC,fr=cycles(fT,Cycle_length);
+#    C = sine_average(fC,100)
     C =np.mean(fC,axis=0);
-        
+    
     fT_detrended=np.concatenate([
         (fC-C).reshape((-1,)),
         fr-C[:len(fr)]
@@ -79,8 +95,7 @@ def recombine_trend_scaled(ft,b,C,nT):
     
     return ft_retrended;
 
-
-def separate_trends(Ftkk,nT,Cycle_length):
+def separate_trends_scaled(Ftkk,nT,Cycle_length):
     (nt,nk1,nk2)=Ftkk.shape;
     
     bkk=np.empty((nk1,nk2,3));
@@ -107,7 +122,7 @@ def recombine_trends(Ftkk_detrended,bkk,Ckk,nT,Cycle_length):
             Ftkk[:,k1,k2] = recombine_trend_scaled(
                 Ftkk_detrended[:,k1,k2],bkk[k1,k2],Ckk[k1,k2],nT,Cycle_length
             )
-    return Ftkk;
+    return Ftkk;a
 
 # Predict from starting point and calculated trend+avg. cycle.
 # Assumes that prediction starts at avg. cycle start
@@ -130,7 +145,7 @@ def kspace_predict_from_trend(f0kk, trend,  t0, pred_length):
     return Ftkk_retrended;
 
 
-def predict_from_trend(training_Ftxx,nT,Cycle_length, pred_length):
+def predict_from_trend_scaled(training_Ftxx,nT,Cycle_length, pred_length):
     (nk1,nk2)     = training_Ftxx.shape[1:]; # For now
     training_Ftkk = dct2(Ftxx,nk1,nk2);
 
@@ -145,5 +160,120 @@ def predict_from_trend(training_Ftxx,nT,Cycle_length, pred_length):
 
     predicted_Ftkk = kspace_predict_from_trend(f0kk,trend_descriptors, t0, pred_length);
 
+
+
+def separate_trend_unscaled(ft,cycle_length):
+    nt = len(ft);
+    
+    # Extract the quadratic trend
+    b=polynomial_trend(ft,2);
+    
+    # Remove trend
+    ts=np.arange(nt);
+    trend=b[0]+b[1]*ts+b[2]*ts*ts;
+    
+    # Remove average cycle
+    fc,fr=cycles(ft,cycle_length);
+    c =np.mean(fc,axis=0);
+    
+    ft_detrended=np.concatenate([
+        (fc-c).reshape((-1,)),
+        fr-c[:len(fr)]
+    ]);
+
+    return (ft_detrended,b,c);
+    
+    
+def separate_trends_unscaled(Ftkk,cycle_length):
+    (nt,nk1,nk2)=Ftkk.shape;
+    
+    bkk=np.empty((nk1,nk2,3));
+    Ckk=np.empty((nk1,nk2,cycle_length));
+    Ftkk_detrended=np.empty(Ftkk.shape);
+
+    # TODO: use vectorized numpy
+    for k1 in range(nk1):
+        for k2 in range(nk2):
+            (ft,b,C) = separate_trend_unscaled(Ftkk[:,k1,k2],cycle_length);
+            Ckk[k1,k2]=C;
+            bkk[k1,k2]=b.flatten();
+            Ftkk_detrended[:,k1,k2]=ft;
+    return (Ftkk_detrended,bkk,Ckk)
+    
+
+def recombine_trend_unscaled(ft,b,C):
+    """Recombine the quadratic trend and average cycle with a de-trended time series.
+    
+    Given the output of separate_trend_scaled() together with the time scale 'nT',
+    this re-combines them into the original time series.
+    """    
+    nt=len(ft);
+    cycle_length=len(C);
+    
+    # Add back quadratic trend
+    ts=np.arange(nt);
+    trend=b[0]+b[1]*ts+b[2]*ts*ts;    
+    
+    # Add back average cycle 
+    fC,fr=cycles(ft,cycle_length);    
+    
+    ft_retrended=np.concatenate([
+        (fC+C).reshape((-1,)),
+        fr+C[:len(fr)]
+    ]);
+    
+    return ft_retrended;
+
+
+def recombine_trends_unscaled(Ftkk_detrended,bkk,Ckk):
+    (nt,nk1,nk2)=Ftkk_detrended.shape;
+    Ftkk=np.empty((nt,nk1,nk2));
+    
+    #TODO: use vectorized numpy
+    for k1 in range(nk1):
+        for k2 in range(nk2):
+            Ftkk[:,k1,k2] = recombine_trend_unscaled(
+                Ftkk_detrended[:,k1,k2],bkk[k1,k2],Ckk[k1,k2]
+            )
+    return Ftkk;
+    
+
+
+def kspace_predict_from_trend_unscaled(f0kk, trend,  pred_ts):
+    (bkk,Ckk,cycle_length) = trend;
+
+    # Quadratic trend for prediction-times. Note: trend is in original time-scale
+    quadratic_trend=bkk[:,:,0,None]+bkk[:,:,1,None]*pred_ts[None,None,:]+bkk[:,:,2,None]*pred_ts[None,None,:]*pred_ts[None,None,:];
+
+    # Get new coefficients for trend, shifting time-scale to t0=0
+    (nk1,nk2) = f0kk.shape;
+    bkk_new=np.array([[polynomial_trend(quadratic_trend[i,j],2) for j in range(nk2)] for i in range(nk1)])
+
+    
+    one = np.ones(len(pred_ts)); # Set Ftkk_detrended to constant f0kk for all t.
+    Ftkk_detrended = f0kk[None,:,:]*one[:,None,None];
+    Ftkk_retrended = recombine_trends_unscaled(Ftkk_detrended,bkk_new,Ckk)
+
+    return Ftkk_retrended;    
+    
+
+def predict_from_trend_unscaled(training_Ftxx,cycle_length,pred_length):
+    (nk1,nk2)     = training_Ftxx.shape[1:]; # For now
+    training_Ftkk = dctn(training_Ftxx,norm='ortho',axes=[1,2])[:,:nk1,:nk2];
+
+    (Ftkk_detrended,bkk,Ckk) =  separate_trends_unscaled(training_Ftkk,cycle_length);
+
+    t0                = training_Ftxx.shape[0];      # Where we start predicting from
+    training_ts       = np.arange(0,t0);    # ts corresponding to training
+    pred_ts           = np.arange(t0,t0+pred_length);
+    
+    trend_descriptors = (bkk,Ckk,cycle_length);
+    f0kk              = Ftkk_detrended[-1];  # We start where the training data ends
+
+    predicted_Ftkk = kspace_predict_from_trend_unscaled(f0kk,trend_descriptors, pred_ts);
+
+    predicted_Ftxx = idctn(predicted_Ftkk,norm='ortho',axes=[1,2]);
+
+    return predicted_Ftxx, predicted_Ftkk;
 
     
