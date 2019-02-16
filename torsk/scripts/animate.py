8b9eff33605c3edf13472514d688fc1446d766f9
Niklas Heim <niklas@threadripper00.dhcp.nbi.dk>
2019-02-16 13:31:47 +0100

    write video with `torsk animate file.nc -v SSH -o out.mp4`

--- torsk/scripts/animate.py
+++ torsk/scripts/animate.py
@@ -2,15 +2,21 @@ import pathlib
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import click
-from torsk.visualize import animate_imshow
+from torsk.visualize import animate_imshow, write_video
 
 
 @click.command("animate", short_help="Animate the SSH component of a .nc file")
-@click.option("--variable", "-v", type=str, default="SSH")
 @click.argument("ncfile", type=pathlib.Path)
-def cli(ncfile, variable):
+@click.option("--variable", "-v", type=str, default="SSH")
+@click.option("--outfile", "-o", type=pathlib.Path, default=None)
+@click.option("--show/--no-show", is_flag=True, default=True)
+def cli(ncfile, variable, outfile, show):
     with nc.Dataset(ncfile, "r") as src:
         data = src[variable][:]
 
-    anim = animate_imshow(data)  # NOQA
-    plt.show()
+    if outfile is not None:
+        write_video(outfile.as_posix(), data)
+
+    if show:
+        anim = animate_imshow(data)  # NOQA
+        plt.show()
