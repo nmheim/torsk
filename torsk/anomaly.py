b655a0f04eef4b5ffbe3eb410121909487fd3edb
Niklas Heim <niklas@threadripper00.dhcp.nbi.dk>
2019-04-26 11:44:59 +0200

    collect means/std in sliding score

--- torsk/anomaly.py
+++ torsk/anomaly.py
@@ -8,17 +8,25 @@ def qfunction(x):
     return 1 - cumulative_distribution(x)
 
 def sliding_score(error, small_window, large_window):
-    shape = (error.shape[0] - large_window,) + error.shape[1:]
-    scores = np.zeros(shape)
+    scores = np.zeros(error.shape)
+    lw_mu = np.zeros_like(scores)
+    lw_std = np.zeros_like(scores)
+    sw_mu = np.zeros_like(scores)
 
-    for i in range(shape[0]):
-        j = i + large_window
-        sw_err = error[j:j+small_window]
-        lw_err = error[j-large_window:j+small_window]
-        mu = lw_err.mean(axis=0)
-        std = lw_err.std(axis=0)
-        sw_mu = sw_err.mean(axis=0)
-        x = np.abs(mu - sw_mu) / std
+    for i in range(small_window, error.shape[0]):
+        end = i + small_window
+        sw_start = max(0, end - small_window)
+        lw_start = max(0, end - large_window)
+
+        sw_err = error[sw_start:end]
+        lw_err = error[lw_start:end]
+
+        lw_mu[i] = lw_err.mean(axis=0)
+        lw_std[i] = lw_err.std(axis=0)
+        sw_mu[i] = sw_err.mean(axis=0)
+
+        x = np.abs(lw_mu[i] - sw_mu[i]) / lw_std[i]
         s = qfunction(x)
         scores[i] = s
-    return scores
+
+    return scores, lw_mu, lw_std, sw_mu
