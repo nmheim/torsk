Fixed size calculation for "compose" input map (#44)

torsk/models/numpy_map_esn.py
@@ -33,10 +33,16 @@ def apply_input_map(image, F):
         _features = image
         for f in F["operations"]:
             _features = apply_input_map(_features, f)
-        F["dbg_size"] = F["operations"][-1]["size"];
+        F["dbg_size"] = hidden_size_of(image.shape,F["operations"][-1])
     else:
         raise ValueError(spec)
-    _features = F["input_scale"] * _features
+
+    if "input_scale" in F.keys():
+        scale = F["input_scale"]
+    else:
+        scale = 1
+    
+    _features = scale * _features
     return _features
 
 
@@ -61,25 +67,30 @@ def init_input_map_specs(input_map_specs, input_shape, dtype):
             spec["bias_ih"] = bias_ih.astype(dtype)
     return input_map_specs
 
-
+def hidden_size_of(input_shape, F):
+    shape = input_shape
+    if F["type"] == "conv":
+        if F["mode"] == "valid":
+            shape = conv2d_output_shape(input_shape, F["size"])
+        elif F["mode"] == "same":
+            size  = shape[0] * shape[1]
+    elif F["type"] == "random_weights":
+        size = F["size"][0]
+    elif F["type"] == "gradient":
+        size = input_shape[0] * input_shape[1] * 2  # For 2d pictures
+    elif F["type"] == "compose":
+        shape = input_shape
+        for f in F["operations"]:
+            size, shape = hidden_size_of(shape,f);
+    else:
+        shape = F["size"]
+        size  = shape[0] * shape[1]
+    return size, shape
+            
 def get_hidden_size(input_shape, input_map_specs):
     hidden_size = 0
-    for spec in input_map_specs:
-        if spec["type"] == "conv":
-            if spec["mode"] == "valid":
-                shape = conv2d_output_shape(input_shape, spec["size"])
-            elif spec["mode"] == "same":
-                shape = input_shape
-                hidden_size += shape[0] * shape[1]
-        elif spec["type"] == "random_weights":
-            hidden_size += spec["size"][0]
-        elif spec["type"] == "gradient":
-            hidden_size += input_shape[0] * input_shape[1] * 2  # For 2d pictures
-        elif spec["type"] == "compose":
-            hidden_size += spec["operations"][-1]["size"]
-        else:
-            shape = spec["size"]
-            hidden_size += shape[0] * shape[1]
+    for F in input_map_specs:
+        hidden_size += hidden_size_of(input_shape,F)[0]
     return hidden_size
 
 

torsk/models/torch_map_esn.py
@@ -12,8 +12,9 @@ from torsk.data.conv import get_kernel as get_np_kernel
 from torsk.models.initialize import dense_esn_reservoir, sparse_nzpr_esn_reservoir
 from torsk.models.numpy_map_esn import get_hidden_size
 
-from torsk.data.utils import resample2d, normalize
-from torsk.data.dct import dct2
+
+#TODO: Use Torch to perform operations instead of NumPy
+from torsk.models.numpy_map_esn import apply_input_map
 
 logger = logging.getLogger(__name__)
 
@@ -22,36 +23,6 @@ def get_kernel(kernel_shape, kernel_type, dtype_str):
     dtype = getattr(torch, dtype_str)
     kernel = get_np_kernel(kernel_shape, kernel_type, dtype_str)
     return torch.tensor(kernel[None, None, :, :], dtype=dtype)
-
-#TODO: Use Torch to perform operations instead of NumPy
-def apply_input_map(image, F):
-    if F["type"] == "pixels":
-        _features = resample2d(image, F["size"]).reshape(-1)
-        F["dbg_size"] = F["size"]
-    elif F["type"] == "dct":
-        _features = dct2(image, *F["size"]).reshape(-1)
-        F["dbg_size"] = F["size"]
-    elif F["type"] == "gradient":
-        grad = np.concatenate(np.gradient(image))
-        _features = normalize(grad.reshape(-1)) * 2 - 1
-        F["dbg_size"] = grad.shape
-    elif F["type"] == "conv":
-        _features = convolve2d(
-            image, F["kernel"], mode='same', boundary="symm")
-        F["dbg_size"] = _features.shape
-        _features = normalize(_features.reshape(-1)) * 2 - 1
-    elif F["type"] == "random_weights":
-        _features = np.dot(F["weight_ih"], image.reshape(-1))
-        _features += F["bias_ih"]
-        F["dbg_size"] = F["size"]
-    elif F["type"] == "compose":
-        _features = image
-        for f in F["operations"]:
-            _features = apply_input_map(_features, f)
-    else:
-        raise ValueError(spec)
-    _features = F["input_scale"] * _features
-    return _features
 
 
 def input_map(image, operations):