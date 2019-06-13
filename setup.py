45e6d07a485d9bdebec88a1861265c8b7d6af064
Niklas Heim <heim.niklas@gmail.com>
2019-06-13 12:05:11 +0200

    only optionally install torch

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup, find_packages
-import os
-
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as fp:
-    install_requires = fp.read()
 
 setup(
     name='torsk',
@@ -11,9 +7,31 @@ setup(
     author_email='heim.niklas@gmail.com',
     packages=find_packages(),
     version=0.1,
-    install_requires=install_requires,
+    install_requires=[
+        "click",
+        "joblib",
+        "marshmallow>=3.0.0b12",
+        "matplotlib",
+        "netCDF4",
+        "numpy",
+        "tqdm",
+        "seaborn",
+        "scikit-optimize",
+        "imageio",
+        "imageio-ffmpeg",
+    ],
     entry_points='''
         [console_scripts]
         torsk=torsk.scripts.cli:cli
-    '''
+    ''',
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-cov"
+        ],
+        "recommended": [
+            "torch",
+            "torchvision"
+        ]
+    }
 )
